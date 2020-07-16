6e5785e12ec9c9459d1771e93282c9b2a3703d1c
Clayton Moore <cmoorefour@gmail.com>
2020-07-16 18:43:12 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+class Dog
+  def initialize(name)
+  @name = dog_name
+end
+
+ def name=(dog_name)
+  @name = dog_name
+end
+
+def name
+   @name
+ end
+end
+
+class Dog
+   def initialize(breed)
+    @breed = breed
+  end
+
+def breed=(breed)
+     @breed = breed
+end
+
+def breed
+     @breed
+   end
+end
